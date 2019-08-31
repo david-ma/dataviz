d4e10bc220e08527e4417a3f99ecd99fb1e4e3d8
David Ma <david.ma@petermac.org>
2019-08-31 17:18:04 +1000

    Sticker app init

--- config.js
+++ config.js
@@ -1,8 +1,40 @@
 var http = require("http");
+var fs = require('fs');
 
 exports.config = {
 	data: true,
 	services: {
+		"upload": function(res, req, db, type){
+			const uploadFolder = "websites/dataviz/data/campjs";
+try {
+			console.log("hey, a request???");
+
+			// var file = new File()
+			// file.
+			console.log(Object.keys(req));
+var buffer = req._readableState.buffer;
+console.log(buffer);
+// 	console.log(req);
+// 	console.log('method', req.method);
+	// console.log(req.connection.parser[2]());
+			// path, flags: string | number
+			// fs.open(uploadFolder+"/test.txt", r);
+
+			// fs.write();
+			// fs.read();
+    fs.writeFileSync(`${uploadFolder}/aaa.txt`, buffer);
+
+    // fs.writeFileSync(`${uploadFolder}/aaa.txt`, JSON.stringify(req._readableState.buffer));
+    // fs.writeFileSync(`${uploadFolder}/bbb.txt`, req._readableState.buffer.head.data);
+
+
+} catch(e) {
+	console.log("error", e);
+}
+			res.end("lol");
+
+
+		},
 		"curl": function(incomingResponse, incomingRequest, db, type) {
 
 			var options = {
