8588a20df7a838975955bd88a3360ea0e1f6d505
David Ma <github@david-ma.net>
2020-11-08 17:08:20 +1100

    Apply standardjs to anomaly.ts

--- src/anomaly/anomaly.ts
+++ src/anomaly/anomaly.ts
@@ -1,53 +1,47 @@
 // import $ from 'jquery';
-import 'datatables.net';
-declare var jsonPath: any;
+import 'datatables.net'
+declare let jsonPath: any
 
-let stuff = [];
-const columnNames = {"gene": "string"};
-
+let stuff = []
+const columnNames = { gene: 'string' }
 
 class Variant {
     gene: string;
 
-    constructor(data) {
-        Object.keys(data.domainModel.schema)
-            .forEach((property) => columnNames[property] = this[property] = jsonPath(data,data.domainModel.schema[property]));
-        if (!this.gene) throw new RangeError("No Gene");
+    constructor (data) {
+      Object.keys(data.domainModel.schema)
+        .forEach((property) => { columnNames[property] = this[property] = jsonPath(data, data.domainModel.schema[property]) })
+      if (!this.gene) throw new RangeError('No Gene')
     }
-
 }
 
-const variants = [];
+const variants = []
 
-$.ajax("/08007755.json", {
-    success: function(d){
-        console.log(d);
-        stuff = d;
+$.ajax('/08007755.json', {
+  success: function (d) {
+    console.log(d)
+    stuff = d
+    globalThis.stuff = stuff
 
-        Object.keys(d.okResults).forEach(function(hgvsg){
-            try {
-                const v = new Variant(d.okResults[hgvsg]);
-                variants.push(v);
-            } catch (e) {
-                if(e.message === 'No Gene') {
-                    // console.log("Variant had no gene. This is fine.");
-                } else {
-                    console.error(e);
-                }
-            }
-        });
+    Object.keys(d.okResults).forEach(function (hgvsg) {
+      try {
+        const v = new Variant(d.okResults[hgvsg])
+        variants.push(v)
+      } catch (e) {
+        if (e.message === 'No Gene') {
+          // console.log("Variant had no gene. This is fine.");
+        } else {
+          console.error(e)
+        }
+      }
+    })
 
-        const columns = Object.keys(columnNames).map( d => ({ data: d, name: d, title: d}));
+    const columns = Object.keys(columnNames).map(d => ({ data: d, name: d, title: d }))
 
-        $("#myTable").DataTable({
-            pageLength: 50,
-            data: variants,
-            columns: columns
-        })
-
-    }
-});
-
-
-
-
+    $('#myTable').DataTable({
+      pageLength: 50,
+      data: variants,
+      columns: columns
+    })
+  }
+})
