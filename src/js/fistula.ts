f6feaafeacd592c103b03098558dcb22f69fe75a
David Ma <github@david-ma.net>
2020-11-30 18:44:15 +1100

    Copy paste box plot code 5 times

--- src/js/fistula.ts
+++ src/js/fistula.ts
@@ -136,6 +136,158 @@ $.when($.ready).then(function () {
       })
 
 
+      // Draw a box plot.
+      data.Incontinent = data.Incontinent.sort((a, b) => {
+        return b.YearsGained - a.YearsGained
+      })
+      var length = data.Incontinent.length
+      var height = chart.height * 0.45
+      var ratio = height / maxYears
+      var top = data.Incontinent[Math.floor(length / 4)].YearsGained * ratio
+      var bottom = data.Incontinent[Math.floor(3 * length / 4)].YearsGained * ratio
+      var midpoint = chart.height / 2
+      chart.svg.append('rect').attrs({
+        x: chart.width * 0.4,
+        width: chart.width * 0.05,
+        y: midpoint - top,
+        height: top - bottom,
+        fill: 'white',
+        stroke: 'black'
+      })
+
+      var median = data.Incontinent[Math.floor(length / 2)].YearsGained * ratio
+      chart.svg.append('line').attrs({
+        x1: chart.width * 0.4,
+        x2: chart.width * 0.45,
+        y1: midpoint - median,
+        y2: midpoint - median,
+        stroke: 'black'
+      })
+
+
+      // Draw a box plot.
+      data.Partial = data.Partial.sort((a, b) => {
+        return b.YearsGained - a.YearsGained
+      })
+      var length = data.Partial.length
+      var height = chart.height * 0.45
+      var ratio = height / maxYears
+      var top = data.Partial[Math.floor(length / 4)].YearsGained * ratio
+      var bottom = data.Partial[Math.floor(3 * length / 4)].YearsGained * ratio
+      var midpoint = chart.height / 2
+      chart.svg.append('rect').attrs({
+        x: chart.width * 0.5,
+        width: chart.width * 0.05,
+        y: midpoint - top,
+        height: top - bottom,
+        fill: 'white',
+        stroke: 'black'
+      })
+
+      var median = data.Partial[Math.floor(length / 2)].YearsGained * ratio
+      chart.svg.append('line').attrs({
+        x1: chart.width * 0.5,
+        x2: chart.width * 0.55,
+        y1: midpoint - median,
+        y2: midpoint - median,
+        stroke: 'black'
+      })
+
+
+
+
+
+
+      // years lost:
+
+      // Draw a box plot.
+      data.Continent = data.Continent.sort((a, b) => {
+        return b.YearsLost - a.YearsLost
+      })
+      var length = data.Continent.length
+      var height = chart.height * 0.45
+      var ratio = height / maxYears
+      var top = data.Continent[Math.floor(length / 4)].YearsLost * ratio
+      var bottom = data.Continent[Math.floor(3 * length / 4)].YearsLost * ratio
+      var midpoint = chart.height / 2
+      chart.svg.append('rect').attrs({
+        x: chart.width * 0.3,
+        width: chart.width * 0.05,
+        y: midpoint + bottom,
+        height: top - bottom,
+        fill: 'white',
+        stroke: 'black'
+      })
+
+      var median = data.Continent[Math.floor(length / 2)].YearsLost * ratio
+      chart.svg.append('line').attrs({
+        x1: chart.width * 0.3,
+        x2: chart.width * 0.35,
+        y1: midpoint + median,
+        y2: midpoint + median,
+        stroke: 'black'
+      })
+
+
+      // Draw a box plot.
+      data.Incontinent = data.Incontinent.sort((a, b) => {
+        return b.YearsLost - a.YearsLost
+      })
+      var length = data.Incontinent.length
+      var height = chart.height * 0.45
+      var ratio = height / maxYears
+      var top = data.Incontinent[Math.floor(length / 4)].YearsLost * ratio
+      var bottom = data.Incontinent[Math.floor(3 * length / 4)].YearsLost * ratio
+      var midpoint = chart.height / 2
+      chart.svg.append('rect').attrs({
+        x: chart.width * 0.4,
+        width: chart.width * 0.05,
+        y: midpoint + bottom,
+        height: top - bottom,
+        fill: 'white',
+        stroke: 'black'
+      })
+
+      var median = data.Incontinent[Math.floor(length / 2)].YearsLost * ratio
+      chart.svg.append('line').attrs({
+        x1: chart.width * 0.4,
+        x2: chart.width * 0.45,
+        y1: midpoint + median,
+        y2: midpoint + median,
+        stroke: 'black'
+      })
+
+
+      // Draw a box plot.
+      data.Partial = data.Partial.sort((a, b) => {
+        return b.YearsLost - a.YearsLost
+      })
+      var length = data.Partial.length
+      var height = chart.height * 0.45
+      var ratio = height / maxYears
+      var top = data.Partial[Math.floor(length / 4)].YearsLost * ratio
+      var bottom = data.Partial[Math.floor(3 * length / 4)].YearsLost * ratio
+      var midpoint = chart.height / 2
+      chart.svg.append('rect').attrs({
+        x: chart.width * 0.5,
+        width: chart.width * 0.05,
+        y: midpoint + bottom,
+        height: top - bottom,
+        fill: 'white',
+        stroke: 'black'
+      })
+
+      var median = data.Partial[Math.floor(length / 2)].YearsLost * ratio
+      chart.svg.append('line').attrs({
+        x1: chart.width * 0.5,
+        x2: chart.width * 0.55,
+        y1: midpoint + median,
+        y2: midpoint + median,
+        stroke: 'black'
+      })
+
+
+      console.log(data);
 
     })
 
