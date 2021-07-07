264c56807806d1c33a363b4d111901874588cfd5
David Ma <david.ma@mcri.edu.au>
2021-07-07 15:16:05 +1000

    Start experimenting with pedigreejs

---
+++ src/js/pedigree.ts
@@ -0,0 +1,17 @@
+import { Chart, decorateTable, $, d3 } from 'chart'
+import 'datatables.net'
+
+import '/pedigreejs/pedigreejs.2.0.0-rc2.min.js'
+
+type pedigreejs = {
+
+}
+
+let pedigreejs = globalThis.pedigreejs
+
+console.log(pedigreejs)
+
+
+
+
+
