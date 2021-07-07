002ad1a654fb4870d424b384987e9ab2844425a0
David Ma <david.ma@mcri.edu.au>
2021-07-07 16:45:59 +1000

    Use example 4 for pedigreejs

--- src/js/pedigree.ts
+++ src/js/pedigree.ts
@@ -6,45 +6,60 @@ import '/pedigreejs/pedigreejs.2.0.0-rc2.min.js'
 type pedigreejs = {
 
 }
+type PedigreeOptions = {
+  targetDiv: string;
+  btn_target: string;
+  width: number;
+  height: number;
+  symbol_size: number;
+  edit: boolean;
+  diseases: {
+    type: string;
+    colour: string;
+  }[];
+  labels: string[];
+  DEBUG: boolean;
+  dataset?: any;
+}
 let pedigreejs = globalThis.pedigreejs
 
 
 
-$( document ).ready(function() {
-  var parent_width = $('#ped').parent().width();
-  var margin = ($(window).width()-parent_width > 10 ? 100 : 30);
-  var svg_width = (parent_width > 750 ? (parent_width*8/12 - margin) : parent_width- margin);
+$(document).ready(function () {
+  var parent_width = $('#pedigrees').parent().width();
+  var margin = ($(window).width() - parent_width > 10 ? 100 : 30);
+  var svg_width = (parent_width > 750 ? (parent_width * 9 / 12 - margin) : parent_width - margin);
 
-  var dataset = [
-    {"name": "m11", "display_name": "John",  "sex": "M", "diabetes_diagnosis_age": 55, "top_level": true},
-    {"name": "f11", "display_name": "Jane",  "sex": "F", "status": 1, "top_level": true},
-    {"name": "m12", "display_name": "Jack", "sex": "M", "top_level": true},
-    {"name": "f12", "display_name": "Jill", "sex": "F", "top_level": true},
-    {"name": "m21", "display_name": "Jim", "sex": "M", "mother": "f11", "father": "m11", "age": 56},
-    {"name": "f21", "display_name": "Jan", "sex": "F", "mother": "f12", "father": "m12", "age": 63},
-    {"name": "ch1", "display_name": "Ana", "sex": "F", "mother": "f21", "father": "m21", "proband": true, "age": 25}
-  ];
-  var opts = {
-    'targetDiv': 'ped',
-    'btn_target': 'history_ex1',
-    'store_type': 'session',
+  var pedfile = "fam1 1 6 7 2 2 0 1 0 0\n" +
+    "fam1 2 4 5 1 1 0 0 0 0\n" +
+    "fam1 3 2 1 2 1 0 0 0 1\n" +
+    "fam1 4 0 0 1 1 0 0 0 0\n" +
+    "fam1 5 0 0 2 2 0 0 0 1\n" +
+    "fam1 6 0 0 1 1 0 0 0 0\n" +
+    "fam1 7 0 0 2 2 0 1 0 1";
+  var dataset = pedigreejs.io.readLinkage(pedfile);
+
+  var opts: PedigreeOptions = {
+    'targetDiv': 'pedigrees',
+    'btn_target': 'history_ex4',
     'width': svg_width,
-    'height': 500,
+    'height': 400,
     'symbol_size': 35,
     'edit': true,
-    'dataset': dataset,
     'diseases': [
-      {'type': 'diabetes', 'colour': '#F68F35'},
+      { 'type': 'diabetes', 'colour': '#F68F35' },
     ],
+    labels: ['famid', 'id', 'alleles'],
     'DEBUG': (pedigreejs.pedigree_utils.urlParam('debug') === null ? false : true)
   };
   $('#opts').append(JSON.stringify(opts, null, 4));
   var local_dataset = pedigreejs.pedcache.current(opts);
   if (local_dataset !== undefined && local_dataset !== null) {
     opts.dataset = local_dataset;
+  } else {
+    opts.dataset = dataset;
   }
-
   opts = pedigreejs.pedigreejs.build(opts);
-  $('#history_ex1').css('max-width', svg_width);
-  $('#history_ex1').css('margin', 'auto');
+  $('#history_ex4').css('max-width', svg_width);
+  $('#history_ex4').css('margin', 'auto');
 });
