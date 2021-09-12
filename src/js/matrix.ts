675503dfa30668e55c3fb70fc36c10a96774e235
David Ma <david.ma@mcri.edu.au>
2021-09-12 18:16:51 +1000

    Get the basic magic numbers needed

--- src/js/matrix.ts
+++ src/js/matrix.ts
@@ -1,7 +1,7 @@
 import { Chart, _, d3 } from 'chart'
 
-var darkgreen = "#223c02",
-    brightgreen = "#98e142"
+const green = "#00c200",
+    brightgreen = "#5ff967"
 
 new Chart({
   element: 'matrix',
@@ -18,8 +18,26 @@ new Chart({
 
   chart.svg.append("text").text("Hello World").attrs({
     x: 2,
-    y: 20,
-    fill: "green"
+    y: 10,
+    fill: green
   });
+
+
+
+  var board = chart.svg.append("g");
+  
+  var word = "Hello World"
+
+  word.split("").forEach((char, col) => {
+    drawChar(1, col, char);
+  })
+
+  function drawChar(line, col, char) {
+    board.append('text').text(char).attrs({
+      x: 2 + (col * 6),
+      y: 10 * (line + 1),
+      fill: green
+    })
+  }
 
 })
