8910fdb8222834659e9898bc60737bd9b8adf48d
David Ma <david.ma@mcri.edu.au>
2021-09-12 17:15:35 +1000

    Formatting

--- config/db_bootstrap.js
+++ config/db_bootstrap.js
@@ -7,24 +7,24 @@ const seq = {
     Blogpost: models_1.Blogpost,
     Scrape: models_1.Scrape,
     Camera: models_1.Camera,
-    Family: models_1.Family
+    Family: models_1.Family,
 };
 if (false) {
     models_1.Camera.findAll({
         where: {
             model: {
-                [sequelize_1.Op.like]: '%Coolpix%'
-            }
-        }
+                [sequelize_1.Op.like]: '%Coolpix%',
+            },
+        },
     }).then(function (cameras) {
         models_1.Family.findOne({
             where: {
-                name: 'Coolpix'
-            }
-        }).then(family => {
-            cameras.forEach(camera => {
+                name: 'Coolpix',
+            },
+        }).then((family) => {
+            cameras.forEach((camera) => {
                 camera.update({
-                    family: family
+                    family: family,
                 });
             });
         });
@@ -34,65 +34,74 @@ if (false) {
     models_1.Family.create({
         brand: 'Nikon',
         name: 'Coolpix',
-        description: 'None'
+        description: 'None',
     });
 }
 if (true) {
-    seq.sequelize.sync({}).then(function (d) {
-        const blogposts = [{
+    seq.sequelize
+        .sync({})
+        .then(function (d) {
+        const blogposts = [
+            {
                 shortname: 'war',
                 title: 'American Wartime',
                 category: '#MakeoverMonday',
                 summary: 'Nearly a quarter of Americans have never experienced the U.S. in a time of peace according to the Washington Post.',
                 image: 'images/war.jpg',
                 publish_date: '2020-02-01',
-                published: true
-            }, {
+                published: true,
+            },
+            {
                 shortname: 'wealth',
                 title: 'World Wealth',
                 category: '#MakeoverMonday',
                 summary: "All of the world's wealth, according to the Credit Suisse report",
                 image: 'images/wealth.png',
                 publish_date: '2020-02-17',
-                published: true
-            }, {
+                published: true,
+            },
+            {
                 shortname: 'influenza',
                 title: 'Influenza Surveillance Report',
                 category: '#MakeoverMonday',
                 summary: 'Influenza in the USA in 2018',
                 image: 'images/influenza.jpg',
                 publish_date: '2018-06-18',
-                published: false
-            }, {
+                published: false,
+            },
+            {
                 shortname: 'homelessness',
                 title: 'Australian homelessness',
                 category: '#MakeoverMonday',
                 summary: 'Housing outcomes for clients of Australian Specialist Homelessness Services',
                 image: 'images/homelessness.png',
                 publish_date: '2020-02-24',
-                published: false
-            }, {
+                published: false,
+            },
+            {
                 shortname: 'kids_sleep',
                 title: "Kids' sleep",
                 category: '#MakeoverMonday',
                 summary: 'Data from savvysleeper.org on how kids sleep',
                 image: 'images/kids_sleep.png',
                 publish_date: '2020-03-02',
-                published: false
-            }, {
+                published: false,
+            },
+            {
                 shortname: 'breathe',
                 title: 'Breathing Polygons',
                 category: '',
                 summary: 'D3.js & maths practice by drawing breathing polygons',
                 image: 'images/breathe.png',
                 publish_date: '2020-11-07',
-                published: true
-            }];
+                published: true,
+            },
+        ];
         blogposts.forEach(function (blogpost) {
             models_1.Blogpost.findOne({
                 where: {
-                    shortname: blogpost.shortname
-                }
+                    shortname: blogpost.shortname,
+                },
             }).then((entry) => {
                 if (!entry) {
                     models_1.Blogpost.create(blogpost);
--- config/db_bootstrap.ts
+++ config/db_bootstrap.ts
@@ -2,123 +2,139 @@ import { Model, Op, Sequelize } from 'sequelize'
 import { dbConfig, Blogpost, Scrape, Camera, Family } from '../models'
 
 export interface seqObject {
-    [key: string] : Model | any | Sequelize;
-    sequelize :Sequelize;
+  [key: string]: Model | any | Sequelize
+  sequelize: Sequelize
 }
 
-const seq :seqObject = {
+const seq: seqObject = {
   sequelize: dbConfig,
   Blogpost: Blogpost,
   Scrape: Scrape,
   Camera: Camera,
-  Family: Family
+  Family: Family,
 }
 
 // Family.get()
-if (false) { // eslint-disable-line
+if (false) {
+  // eslint-disable-line
   Camera.findAll({
     where: {
       model: {
-        [Op.like]: '%Coolpix%'
-      }
-    }
+        [Op.like]: '%Coolpix%',
+      },
+    },
   }).then(function (cameras) {
     Family.findOne({
       where: {
-        name: 'Coolpix'
-      }
-    }).then(family => {
-      cameras.forEach(camera => {
+        name: 'Coolpix',
+      },
+    }).then((family) => {
+      cameras.forEach((camera) => {
         // camera.setFamily(family);
         // camera.addFamily(family);
         // console.log(camera);
         camera.update({
-          family: family
+          family: family,
         })
       })
     })
   })
 }
 
-if (false) { // eslint-disable-line
+if (false) {
+  // eslint-disable-line
   Family.create({
     brand: 'Nikon',
     name: 'Coolpix',
-    description: 'None'
+    description: 'None',
   })
 }
 
 // rebuild entire database & reload data..?
-if (true) { // eslint-disable-line
-  seq.sequelize.sync({
-    // force: true
-  }).then(function (d) { // eslint-disable-line
-    // Add blog posts
-    const blogposts = [{
-      shortname: 'war',
-      title: 'American Wartime',
-      category: '#MakeoverMonday',
-      summary: 'Nearly a quarter of Americans have never experienced the U.S. in a time of peace according to the Washington Post.',
-      image: 'images/war.jpg',
-      publish_date: '2020-02-01',
-      published: true
-    }, {
-      shortname: 'wealth',
-      title: 'World Wealth',
-      category: '#MakeoverMonday',
-      summary: "All of the world's wealth, according to the Credit Suisse report",
-      image: 'images/wealth.png',
-      publish_date: '2020-02-17',
-      published: true
-    }, {
-      shortname: 'influenza',
-      title: 'Influenza Surveillance Report',
-      category: '#MakeoverMonday',
-      summary: 'Influenza in the USA in 2018',
-      image: 'images/influenza.jpg',
-      publish_date: '2018-06-18',
-      published: false
-    }, {
-      shortname: 'homelessness',
-      title: 'Australian homelessness',
-      category: '#MakeoverMonday',
-      summary: 'Housing outcomes for clients of Australian Specialist Homelessness Services',
-      image: 'images/homelessness.png',
-      publish_date: '2020-02-24',
-      published: false
-    }, {
-      shortname: 'kids_sleep',
-      title: "Kids' sleep",
-      category: '#MakeoverMonday',
-      summary: 'Data from savvysleeper.org on how kids sleep',
-      image: 'images/kids_sleep.png',
-      publish_date: '2020-03-02',
-      published: false
-    }, {
-      shortname: 'breathe',
-      title: 'Breathing Polygons',
-      category: '',
-      summary: 'D3.js & maths practice by drawing breathing polygons',
-      image: 'images/breathe.png',
-      publish_date: '2020-11-07',
-      published: true
-    }]
+if (true) {
+  // eslint-disable-line
+  seq.sequelize
+    .sync({
+      // force: true
+    })
+    .then(function (d) {
+      // eslint-disable-line
+      // Add blog posts
+      const blogposts = [
+        {
+          shortname: 'war',
+          title: 'American Wartime',
+          category: '#MakeoverMonday',
+          summary:
+            'Nearly a quarter of Americans have never experienced the U.S. in a time of peace according to the Washington Post.',
+          image: 'images/war.jpg',
+          publish_date: '2020-02-01',
+          published: true,
+        },
+        {
+          shortname: 'wealth',
+          title: 'World Wealth',
+          category: '#MakeoverMonday',
+          summary:
+            "All of the world's wealth, according to the Credit Suisse report",
+          image: 'images/wealth.png',
+          publish_date: '2020-02-17',
+          published: true,
+        },
+        {
+          shortname: 'influenza',
+          title: 'Influenza Surveillance Report',
+          category: '#MakeoverMonday',
+          summary: 'Influenza in the USA in 2018',
+          image: 'images/influenza.jpg',
+          publish_date: '2018-06-18',
+          published: false,
+        },
+        {
+          shortname: 'homelessness',
+          title: 'Australian homelessness',
+          category: '#MakeoverMonday',
+          summary:
+            'Housing outcomes for clients of Australian Specialist Homelessness Services',
+          image: 'images/homelessness.png',
+          publish_date: '2020-02-24',
+          published: false,
+        },
+        {
+          shortname: 'kids_sleep',
+          title: "Kids' sleep",
+          category: '#MakeoverMonday',
+          summary: 'Data from savvysleeper.org on how kids sleep',
+          image: 'images/kids_sleep.png',
+          publish_date: '2020-03-02',
+          published: false,
+        },
+        {
+          shortname: 'breathe',
+          title: 'Breathing Polygons',
+          category: '',
+          summary: 'D3.js & maths practice by drawing breathing polygons',
+          image: 'images/breathe.png',
+          publish_date: '2020-11-07',
+          published: true,
+        },
+      ]
 
-    blogposts.forEach(function (blogpost) {
-      // console.log(`Adding ${blogpost.shortname}`)
-      Blogpost.findOne({
-        where: {
-          shortname: blogpost.shortname
-        }
-      }).then((entry) => {
-        if (!entry) {
-          Blogpost.create(blogpost)
-        } else {
-          entry.update(blogpost)
-        }
+      blogposts.forEach(function (blogpost) {
+        // console.log(`Adding ${blogpost.shortname}`)
+        Blogpost.findOne({
+          where: {
+            shortname: blogpost.shortname,
+          },
+        }).then((entry) => {
+          if (!entry) {
+            Blogpost.create(blogpost)
+          } else {
+            entry.update(blogpost)
+          }
+        })
       })
     })
-  })
 }
 
 exports.seq = seq
