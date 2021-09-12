359d45a6d926dd1d25a4be56a513ff4c381098ab
David Ma <david.ma@mcri.edu.au>
2021-09-12 19:34:11 +1000

    Add column class

--- src/js/matrix.ts
+++ src/js/matrix.ts
@@ -3,15 +3,62 @@ import { Chart, _, d3 } from 'chart'
 const green = '#00c200',
   brightgreen = '#5ff967'
 
+const charset =
+  'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%^&*()'.split(
+    ''
+  )
+
+function randomChar() {
+  return charset[Math.floor(Math.random() * charset.length)]
+}
+
+type Board = d3.Selection<SVGSVGElement, any, HTMLElement, any>
+class Column {
+  board: Board
+  lines: number
+  col: number
+  curLine: number
+
+  constructor(opts: { col: number; lines: number; board: Board }) {
+    this.col = opts.col
+    this.lines = opts.lines
+    this.board = opts.board
+    this.curLine = 0
+    this.step()
+  }
+
+  step() {
+    const letter = randomChar()
+    this.drawChar(this.curLine, this.col, letter)
+    this.curLine++
+  }
+
+  drawChar(line, col, char) {
+    this.board
+      .append('text')
+      .text(char)
+      .attrs({
+        x: 2 + col * 6,
+        y: 10 * (line + 1),
+        fill: green,
+      })
+  }
+}
 class Matrix {
   columns: Array<number>
 
-  constructor(opts: {
-    board: d3.Selection<SVGSVGElement, any, HTMLElement, any>
-    lines: number
-    cols: number
-  }) {
+  constructor(opts: { board: Board; lines: number; cols: number }) {
     // this.columns = [cols]
+
+    var testCol = new Column({
+      col: 5,
+      lines: opts.lines,
+      board: opts.board,
+    })
+
+    setInterval(function () {
+      testCol.step()
+    }, 40)
   }
 
   addRandomDrop() {}
@@ -36,8 +83,7 @@ new Chart({
   var lines = Math.floor(chart.innerHeight / 10), // 60
     cols = Math.floor(chart.innerWidth / 6) // 160
 
-  var board: d3.Selection<SVGSVGElement, any, HTMLElement, any> =
-    chart.svg.append('g')
+  var board: Board = chart.svg.append('g')
 
   var matrix = new Matrix({
     lines: lines,
