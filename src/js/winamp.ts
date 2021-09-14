8379116339da1b2259bf1080697f30abe09830da
David Ma <david.ma@mcri.edu.au>
2021-09-15 03:27:47 +1000

    Improve the winamp viz

--- src/js/winamp.ts
+++ src/js/winamp.ts
@@ -1,11 +1,14 @@
 import { Chart, d3 } from 'chart'
 
+const speed = 3000
+
 new Chart({
   element: 'viz',
   margin: 1,
+  height: 400,
+  width: 600,
   nav: false,
 }).scratchpad((chart) => {
-
   chart.svg
     .selectAll('line')
     .data(new Array(50))
@@ -17,24 +20,23 @@ new Chart({
         y1: 0,
         x2: i * 15,
         y2: chart.innerHeight,
-        stroke: 'black',
       }
     })
 
   function animate() {
-    const top = Math.random() * 50,
-      bottom = Math.random() * 50,
+    const top = (Math.random() - 0.5) * 50,
+      bottom = (Math.random() - 0.5) * 50,
       offset_top = Math.random() * 300,
       offset_bottom = Math.random() * 300
     chart.svg
       .selectAll('line')
       .transition()
       .ease(d3.easeQuad)
-      .duration(3000)
+      .duration(speed)
       .attrs((d, i) => {
         return {
-          x1: i * top + offset_top,
-          x2: i * bottom + offset_bottom,
+          x1: i * top + offset_top + i * i * 0.1,
+          x2: i * bottom + offset_bottom + i * i * 0.1,
         }
       })
   }
@@ -43,5 +45,5 @@ new Chart({
 
   window.setInterval(function () {
     animate()
-  }, 3000)
+  }, speed)
 })
