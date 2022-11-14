ff34347d8ca53b15128b4fff34cd69a9143a44a3
David Ma <david.ma@mcri.edu.au>
2022-11-14 13:48:25 +1100

    Save progress

--- src/js/atlassian.ts
+++ src/js/atlassian.ts
@@ -8,41 +8,114 @@ function fetchAllFeatures() {
   // `fetch("/api/features/all")`
   return new Promise((resolve) => {
     const sampleFeatures = {
-      'extended-summary': true,
-      'feedback-dialog': false,
+      'unknown-feature': {
+        default: false,
+        nav: null,
+      },
+      'extended-summary': {
+        default: true,
+      },
+      'feedback-dialog': {
+        default: false,
+      },
     }
 
     setTimeout(resolve, 100, sampleFeatures)
+    // throw new Error('Broken')
   })
 }
 
-function getFeatureState(featureName) {
+let globalFeatures = {}
+let globalFeaturesTimestamp = null
+
+function getFeatureState(featureName, component = 'default') {
+  const defaults = {
+    'unknown-feature': {
+      default: false,
+    },
+    'extended-summary': {
+      default: true,
+    },
+    'feedback-dialog': {
+      default: true,
+    },
+  }
+
   // To be implemented by you
   // this function would use fetchAllFeatures
 
   return new Promise((resolve) => {
-    fetchAllFeatures().then(function (features) {
-      return resolve(features[featureName])
-    })
+    // Check the timestamp of the cached features
+    // Use the cached features if they are less than 5 minutes old
+
+    // if (globalFeaturesTimestamp && globalFeaturesTimestamp > Date.now() - 1000 * 60 * 5) {
+    if (globalFeaturesTimestamp) {
+      console.log('Using cached features')
+      resolve(globalFeatures[featureName][component])
+    } else {
+      fetchAllFeatures()
+        .catch((error) => {
+          // handle error.
+          // probably assume features are all disabled
+          // Log the error
+
+          resolve(defaults[featureName][component])
+        })
+
+        .then(function (features) {
+          globalFeatures = features
+          globalFeaturesTimestamp = true
+
+          if (features[featureName]) {
+            if (Object.keys(features[featureName]).indexOf('component') > -1) {
+              return resolve(features[featureName][component])
+            } else {
+              return resolve(features[featureName].default)
+            }
+          } else {
+            return resolve(defaults['unknown-feature'][component])
+          }
+        })
+    }
   })
 }
 
+
+
+
 // // Function usage examples
 // // src/feature-x/summary.js
-getFeatureState('extended-summary').then(function (isEnabled) {
+getFeatureState('extended-summary', 'homepage').then(function (isEnabled) {
   if (isEnabled) {
-    console.log("extended summary is enabled")
+    console.log('extended summary is enabled')
     // showExtendedSummary();
   } else {
-    console.log("showing brief summary")
+    console.log('showing brief summary')
     // showBriefSummary();
   }
 })
 
 // // src/feature-y/feedback-dialog.js
-getFeatureState("feedback-dialog")
-  .then(function(isEnabled) {
+getFeatureState('feedback-dialog').then(function (isEnabled) {
+  console.log('Feedback dialog', isEnabled)
+
+  if (isEnabled) {
+    console.log('feedback dialog is enabled')
+    // makeFeedbackButtonVisible();
+  }
+})
+
+
+
+
+setTimeout(function () {
+  // // src/feature-y/feedback-dialog.js
+  getFeatureState('feedback-dialog').then(function (isEnabled) {
+    console.log('Feedback dialog', isEnabled)
+
     if (isEnabled) {
+      console.log('feedback dialog is enabled')
       // makeFeedbackButtonVisible();
     }
-  });
+  })
+}, 1000)
