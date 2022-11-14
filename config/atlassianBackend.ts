8a362a7a600321d024ef16cf1fda88ffbbb5dd2f
David Ma <david.ma@mcri.edu.au>
2022-11-14 13:06:35 +1100

    delete unused code

--- config/atlassianBackend.ts
+++ config/atlassianBackend.ts
@@ -1,67 +1,6 @@
 import { Thalia } from '../../../server/thalia'
 
-const backendData = [
-  {
-    id: '1',
-    name: 'Office Map',
-  },
-  {
-    id: '2',
-    name: 'New Employee Onboarding',
-    children: [
-      {
-        id: '8',
-        name: 'Onboarding Materials',
-      },
-      {
-        id: '9',
-        name: 'Training',
-      },
-    ],
-  },
-  {
-    id: '3',
-    name: 'Office Events',
-    children: [
-      {
-        id: '6',
-        name: '2018',
-        children: [
-          {
-            id: '10',
-            name: 'Summer Picnic',
-          },
-          {
-            id: '11',
-            name: "Valentine's Day Party",
-          },
-          {
-            id: '12',
-            name: "New Year's Party",
-          },
-        ],
-      },
-      {
-        id: '7',
-        name: '2017',
-        children: [
-          {
-            id: '13',
-            name: 'Company Anniversary Celebration',
-          },
-        ],
-      },
-    ],
-  },
-  {
-    id: '4',
-    name: 'Public Holidays',
-  },
-  {
-    id: '5',
-    name: 'Vacations and Sick Leaves',
-  },
-]
+
 
 let config: Thalia.WebsiteConfig = {
   services: {
@@ -69,9 +8,6 @@ let config: Thalia.WebsiteConfig = {
       console.log('Hey atlassian, the time is ' + new Date().toString())
 
       res.end('Hello world!')
-    },
-    backendData: function (res, req, db) {
-      res.end(JSON.stringify(backendData))
     },
   },
 }
