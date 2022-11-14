ef68e22c00fbc13773fcda1baeefec848dc40470
David Ma <david.ma@mcri.edu.au>
2022-11-14 12:45:42 +1100

    Move backend data to the backend

--- src/js/atlassian.ts
+++ src/js/atlassian.ts
@@ -4,75 +4,6 @@ console.log('This message is coming from atlassian.ts')
 
 // Arrow characters to use: ▼ ▶ •
 
-const backendData = [
-  {
-    id: '1',
-    name: 'Office Map',
-  },
-  {
-    id: '2',
-    name: 'New Employee Onboarding',
-    children: [
-      {
-        id: '8',
-        name: 'Onboarding Materials',
-      },
-      {
-        id: '9',
-        name: 'Training',
-      },
-    ],
-  },
-  {
-    id: '3',
-    name: 'Office Events',
-    children: [
-      {
-        id: '6',
-        name: '2018',
-        children: [
-          {
-            id: '10',
-            name: 'Summer Picnic',
-          },
-          {
-            id: '11',
-            name: "Valentine's Day Party",
-          },
-          {
-            id: '12',
-            name: "New Year's Party",
-          },
-        ],
-      },
-      {
-        id: '7',
-        name: '2017',
-        children: [
-          {
-            id: '13',
-            name: 'Company Anniversary Celebration',
-          },
-        ],
-      },
-    ],
-  },
-  {
-    id: '4',
-    name: 'Public Holidays',
-  },
-  {
-    id: '5',
-    name: 'Vacations and Sick Leaves',
-  },
-]
-
-function fetchData() {
-  return new Promise((resolve) => {
-    setTimeout(resolve, 100, backendData)
-  })
-}
-
 function drawNode(list_element, node) {
   if (node.children) {
     list_element.classed('hasChildren', true)
@@ -99,11 +30,13 @@ function drawNode(list_element, node) {
   }
 }
 
-fetchData().then((d: any) => {
-  const list = d3.select('#list')
+fetch('/backendData')
+  .then((d) => d.json())
+  .then((d: any) => {
+    const list = d3.select('#list')
 
-  d.forEach(function (node) {
-    const li = list.append('li')
-    drawNode(li, node)
+    d.forEach(function (node) {
+      const li = list.append('li')
+      drawNode(li, node)
+    })
   })
-})
