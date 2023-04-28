710444bcb43085364f77c60b6ecbc6c806b028ea
David Ma <hello@david-ma.net>
2023-04-28 10:35:08 +1000

    Log the photo scraping better using promises

--- config/scripts/xray_test.js
+++ config/scripts/xray_test.js
@@ -14,41 +14,52 @@ function xray(html) {
             photos: 0,
             newPhotos: 0,
         };
-        projects.forEach((project) => {
-            x(html, `article.project[data-id=${project}]`, {
-                project: project,
-                photos: x(`a[rel="project-${project}-images"]`, [
-                    {
-                        url: '@href',
-                        caption: '@title',
-                    },
-                ]),
-            })(function (err, blob) {
-                if (err) {
-                    console.log('ERROR', err);
-                }
-                blob.photos.forEach((photo) => {
-                    photo.awesome_project_id = project;
-                    tally.photos++;
-                    AwesomePhoto.findOne({
-                        where: {
-                            url: photo.url,
+        Promise.all(projects.map((project) => {
+            new Promise((resolve, reject) => {
+                x(html, `article.project[data-id=${project}]`, {
+                    project: project,
+                    photos: x(`a[rel="project-${project}-images"]`, [
+                        {
+                            url: '@href',
+                            caption: '@title',
                         },
-                    }).then((d) => {
-                        if (d) {
-                        }
-                        else {
-                            tally.newPhotos++;
-                            AwesomePhoto.create(photo).catch((error) => {
-                                console.log('Error', error);
+                    ]),
+                })(function (err, blob) {
+                    if (err) {
+                        console.log('ERROR', err);
+                        reject(err);
+                    }
+                    Promise.all(blob.photos.map((photo) => {
+                        new Promise((resolve, reject) => {
+                            photo.awesome_project_id = project;
+                            tally.photos++;
+                            AwesomePhoto.findOne({
+                                where: {
+                                    url: photo.url,
+                                },
+                            }).then((d) => {
+                                if (d) {
+                                }
+                                else {
+                                    tally.newPhotos++;
+                                    AwesomePhoto.create(photo).catch((error) => {
+                                        console.log('Error', error);
+                                    });
+                                }
+                                resolve('done');
                             });
-                        }
+                        });
+                    })).then((d) => {
+                        resolve(blob);
                     });
                 });
             });
+        })).then((d) => {
+            console.log('Done!');
+            console.log(`Total projects: ${tally.total}`);
+            console.log(`Total photos found: ${tally.photos}`);
+            console.log(`New photos: ${tally.newPhotos}`);
         });
-        console.log('Done!');
-        console.log(`Total projects: ${tally.total}`);
     });
 }
 exports.xray = xray;
--- config/scripts/xray_test.ts
+++ config/scripts/xray_test.ts
@@ -19,49 +19,62 @@ export function xray(html) {
     }
 
     // Get the photos for each project
-    projects.forEach((project) => {
-      x(html, `article.project[data-id=${project}]`, {
-        project: project,
-        photos: x(`a[rel="project-${project}-images"]`, [
-          {
-            url: '@href',
-            caption: '@title',
-          },
-        ]),
-      })(function (err, blob) {
-        if (err) {
-          console.log('ERROR', err)
-        }
-        // console.log(`Project ${project}:`, blob)
-        // Save the photos to the database
-        blob.photos.forEach((photo) => {
-          photo.awesome_project_id = project
-          tally.photos++
-
-          AwesomePhoto.findOne({
-            where: {
-              url: photo.url,
-            },
-          }).then((d) => {
-            if (d) {
-              // console.log('Found existing record', d.id)
-              // d.update(photo)
-            } else {
-              tally.newPhotos++
-              // console.log('Creating new record', photo.url)
-              AwesomePhoto.create(photo).catch((error) => {
-                console.log('Error', error)
-              })
+    Promise.all(
+      projects.map((project) => {
+        new Promise((resolve, reject) => {
+          x(html, `article.project[data-id=${project}]`, {
+            project: project,
+            photos: x(`a[rel="project-${project}-images"]`, [
+              {
+                url: '@href',
+                caption: '@title',
+              },
+            ]),
+          })(function (err, blob) {
+            if (err) {
+              console.log('ERROR', err)
+              reject(err)
             }
+
+            // console.log(`Project ${project}:`, blob)
+            // Save the photos to the database
+            Promise.all(
+              blob.photos.map((photo) => {
+                new Promise((resolve, reject) => {
+                  photo.awesome_project_id = project
+                  tally.photos++
+
+                  AwesomePhoto.findOne({
+                    where: {
+                      url: photo.url,
+                    },
+                  }).then((d) => {
+                    if (d) {
+                      // console.log('Found existing record', d.id)
+                      // d.update(photo)
+                    } else {
+                      tally.newPhotos++
+                      // console.log('Creating new record', photo.url)
+                      AwesomePhoto.create(photo).catch((error) => {
+                        console.log('Error', error)
+                      })
+                    }
+                    resolve('done')
+                  })
+                })
+              })
+            ).then((d) => {
+              resolve(blob)
+            })
           })
         })
       })
+    ).then((d) => {
+      console.log('Done!')
+      console.log(`Total projects: ${tally.total}`)
+      // This tally doesn't work because the xray & sequelize calls are asynchronous
+      console.log(`Total photos found: ${tally.photos}`)
+      console.log(`New photos: ${tally.newPhotos}`)
     })
-
-    console.log('Done!')
-    console.log(`Total projects: ${tally.total}`)
-    // This tally doesn't work because the xray & sequelize calls are asynchronous
-    // console.log(`Total photos found: ${tally.photos}`)
-    // console.log(`New photos: ${tally.newPhotos}`)
   })
 }
