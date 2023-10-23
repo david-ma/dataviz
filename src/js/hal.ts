6a3bcf5d4009199baa83e064f6ed11b18d091d36
David Ma <hello@david-ma.net>
2023-10-23 23:50:39 +1100

    Linting

--- src/js/hal.ts
+++ src/js/hal.ts
@@ -1,37 +1,38 @@
 // @ts-nocheck
 
-console.log("Hello World");
+console.log('Hello World')
 
 const height = 600,
-      width = 960
+  width = 960
 
-const svg = d3.select("#screen").append("svg")
+const svg = d3
+  .select('#screen')
+  .append('svg')
   .attrs({
-    viewBox: `0 0 ${width} ${height}`
-  });
+    viewBox: `0 0 ${width} ${height}`,
+  })
 
-svg.append("rect")
-  .attrs({
-    x: 0,
-    y: 0,
-    height: height,
-    width: width,
-    fill: '#8357a4',
-  });
+svg.append('rect').attrs({
+  x: 0,
+  y: 0,
+  height: height,
+  width: width,
+  fill: '#8357a4',
+})
 
 const offset = 100
 
-const banner = svg.append("rect")
-  .attrs({
-    x: offset,
-    y: 50,
-    height: 100,
-    width: width - (offset * 2),
-    fill: 'white'
-  })
+const banner = svg.append('rect').attrs({
+  x: offset,
+  y: 50,
+  height: 100,
+  width: width - offset * 2,
+  fill: 'white',
+})
 
-svg.append("text")
-  .text("HELLO WORLD")
+svg
+  .append('text')
+  .text('HELLO WORLD')
   .attrs({
     x: width / 2,
     y: 120,
@@ -40,23 +41,17 @@ svg.append("text")
     'text-anchor': 'middle',
   })
 
-
-globalThis.aniamteButton = function() {
-  console.log("animate button clicked");
-  banner.attrs({
-    x: width / 2,
-    width: 0,
-  })
-  .transition()
-  .duration(1500)
-  .attrs({
-    x: offset,
-    width: width - (offset * 2),
-  })
-
-
+globalThis.aniamteButton = function () {
+  console.log('animate button clicked')
+  banner
+    .attrs({
+      x: width / 2,
+      width: 0,
+    })
+    .transition()
+    .duration(1500)
+    .attrs({
+      x: offset,
+      width: width - offset * 2,
+    })
 }
-
-
-
-
