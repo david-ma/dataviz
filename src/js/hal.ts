f517a6ad636bdd231ef0072f6083c408eadaad1d
David Ma <hello@david-ma.net>
2023-10-25 23:37:26 +1100

    Progress on a screen

--- src/js/hal.ts
+++ src/js/hal.ts
@@ -7,6 +7,7 @@ const height = 600,
 
 const screens = d3.select('#screens')
 
+// Screen 1
 const svg = screens.append('svg').attrs({
   viewBox: `0 0 ${width} ${height}`,
 })
@@ -39,6 +40,89 @@ svg
     'font-size': '48px',
     'text-anchor': 'middle',
   })
+
+svg.style('display', 'none')
+
+const screen2 = screens.append('svg').attrs({
+  viewBox: `0 0 ${width} ${height}`,
+})
+
+screen2.append('rect').attrs({
+  x: 0,
+  y: 0,
+  height: height,
+  width: width,
+  fill: '#b91321',
+})
+
+screen2.append('rect').attrs({
+  x: width / 3, // third
+  y: height / 4,
+  height: height / 3,
+  width: width / 3,
+  stroke: 'white',
+  fill: 'none',
+  'stroke-width': 0.5,
+})
+
+var lines = []
+for (var i = 0; i < 10; i++) {
+  var top = height / 4
+  var bottom = 2 * height / 4
+
+  lines.push({
+    x1: width / 3,
+    x2: width / 3,
+    y1: top,
+    y2: bottom,
+  })
+}
+
+lines.forEach((line) => {
+  screen2
+    .append('line')
+    .attrs({
+      x1: line.x1,
+      y1: line.y1,
+      x2: line.x2,
+      y2: line.y2,
+      stroke: 'white',
+      'stroke-width': 0.5,
+    })
+})
+
+
+
+// 4x4 block of text
+var text = []
+for (var i = 0; i < 4; i++) {
+  text.push([
+    '0' + Math.floor(Math.random() * 1000000000),
+    '0' + Math.floor(Math.random() * 1000000000),
+    '0' + Math.floor(Math.random() * 1000000000),
+    '0' + Math.floor(Math.random() * 1000000000),
+  ])
+}
+
+var textY = (height / 10) * 7
+text.forEach((row, i) => {
+  console.log("Row", row)
+  row.forEach((number, j) => {
+    screen2
+    .append('text')
+    .text(number)
+    .attrs({
+      x: width / 3 + j * 100,
+      y: textY + i * 15,
+      fill: 'white',
+      'font-size': '10px',
+      'text-anchor': 'start',
+      // Lucida Console???
+      'font-family': 'Lucida Grande, monospace',
+      // 'font-family': 'monospace',
+    })
+  })
+})
 
 globalThis.aniamteButton = function () {
   console.log('animate button clicked')
