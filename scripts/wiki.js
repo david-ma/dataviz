dea9e0d6f0bcdf13c21caeeaa7e63ca23471e4c0
David Ma <hello@david-ma.net>
2023-12-18 20:05:31 +1100

    Working script

--- scripts/wiki.js
+++ scripts/wiki.js
@@ -1,183 +1,73 @@
 const axios = require('axios'),
   fs = require('fs')
 
-if (axios && fs) {
-  console.log('axios and fs are loaded')
-}
 // console.log("hello world", fs)
 // console.log("hello world", axios)
 
-// Open /data/ship_of_theseus_revisions2.json
-
-fs.readFile('data/ship_of_theseus_revisions_3.json', 'utf8', (err, data) => {
-  var string = JSON.parse(data)
-  console.log(string)
-})
-
-async function getArticleRevisions(title, maxRevisions) {
-  const apiUrl = `https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=${title}&rvprop=timestamp|user|comment|content&rvlimit=${maxRevisions}&redirects=1&rvdir=newer`;
+async function getArticleRevisions(title, maxRevisions, newestId) {
+  console.log('newestId', newestId)
+  const apiUrl = `https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=${title}&rvprop=ids|tags|timestamp|user|comment|content&rvlimit=${maxRevisions}&redirects=1&rvdir=newer&rvstartid=${parseInt(newestId) + 1}`
 
   try {
-    const response = await axios.get(apiUrl);
-    const pages = response.data.query.pages;
+    const response = await axios.get(apiUrl)
+    const pages = response.data.query.pages
 
     // Extract revisions from the API response
-    const revisions = Object.values(pages)[0]?.revisions || [];
+    const revisions = Object.values(pages)[0]?.revisions || []
 
-    return revisions.map(revision => ({
+    console.log(revisions)
+
+    return revisions.map((revision) => ({
       id: revision.revid,
       timestamp: revision.timestamp,
       comment: revision.comment,
+      contentformat: revision.contentformat,
+      contentmodel: revision.contentmodel,
+      tags: revision.tags,
       user: revision.user || 'Anonymous', // If the user is not available, set to 'Anonymous'
-      content: revision['*']
-    }));
+      content: revision['*'],
+    }))
   } catch (error) {
-    console.error('Error fetching data:', error.message);
-    return [];
+    console.error('Error fetching data:', error.message)
+    return []
   }
 }
 
 function saveRevisionsToFile(revisions, filename) {
-  const data = JSON.stringify(revisions, null, 1);
+  const data = JSON.stringify(revisions, null, 1)
+  console.log('Writing to file', revisions.length)
 
   fs.writeFile(filename, data, (err) => {
     if (err) {
-      console.error('Error saving revisions to file:', err.message);
+      console.error('Error saving revisions to file:', err.message)
     } else {
-      console.log(`Revisions saved to ${filename}`);
+      console.log(`Revisions saved to ${filename}`)
     }
-  });
+  })
 }
 
-const articleTitle = 'Ship_of_Theseus';
-const maxRevisions = 1000;
-const outputFilename = 'data/ship_of_theseus_revisions_7.json';
+const articleTitle = 'Ship_of_Theseus'
+const maxRevisions = 50
+const outputFilename = 'data/ship_of_theseus_revisions.json'
 
-(async () => {
-  const revisions = await getArticleRevisions(articleTitle, maxRevisions);
-  saveRevisionsToFile(revisions, outputFilename);
-})();
+;(async () => {
+  var newestId = 0
+  var records = []
 
-// const diff = require('diff');
+  fs.readFile(outputFilename, 'utf8', (err, data) => {
+    records = JSON.parse(data)
+    records.forEach((record) => {
+      if (record.id > newestId) {
+        newestId = record.id
+      }
+    })
 
-// async function getArticleRevisions(title, batchSize, totalRevisions) {
-//   let revisions = [];
+    getArticleRevisions(articleTitle, maxRevisions, newestId).then((revisions) => {
+      saveRevisionsToFile(
+        records.concat(revisions),
+        outputFilename
+      )
+    })
 
-//   const totalPages = Math.ceil(totalRevisions / batchSize);
-
-//   for (let page = 0; page < totalPages; page++) {
-//     const apiUrl = `https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=${title}&rvprop=timestamp|user|content&rvlimit=${batchSize}&rvdir=newer&rvstart=${(page * batchSize) + 1}`;
-
-//     try {
-//       const response = await axios.get(apiUrl);
-//       const pages = response.data.query.pages;
-
-//       // Extract revisions from the API response
-//       const pageRevisions = Object.values(pages)[0]?.revisions || [];
-//       revisions = revisions.concat(pageRevisions.map(revision => ({
-//         timestamp: revision.timestamp,
-//         user: revision.user || 'Anonymous',
-//         content: revision['*']
-//       })));
-
-//     } catch (error) {
-//       console.error('Error fetching data:', error.message);
-//       return [];
-//     }
-//   }
-
-//   return revisions;
-// }
-
-// function highlightChanges(previousContent, currentContent) {
-//   // ... (unchanged)
-// }
-
-// function saveRevisionsToFile(revisions, filename) {
-//   const formattedRevisions = revisions.map((revision, index) => {
-//     if (index > 0) {
-//       const previousContent = revisions[index - 1].content;
-//       revision.highlightedContent = highlightChanges(previousContent, revision.content);
-//     }
-//     return revision;
-//   });
-
-//   const data = JSON.stringify(formattedRevisions, null, 2);
-
-//   fs.writeFile(filename, data, (err) => {
-//     if (err) {
-//       console.error('Error saving revisions to file:', err.message);
-//     } else {
-//       console.log(`Revisions saved to ${filename}`);
-//     }
-//   });
-// }
-
-// const articleTitle = 'Ship_of_Theseus';
-// const batchSize = 50; // Adjust the batch size as needed
-// const totalRevisions = 500; // Adjust the total number of revisions you want to download
-// const outputFilename = 'ship_of_theseus_revisions.json';
-
-// (async () => {
-//   const revisions = await getArticleRevisions(articleTitle, batchSize, totalRevisions);
-//   saveRevisionsToFile(revisions, outputFilename);
-// })();
-
-// const axios = require('axios');
-// const fs = require('fs');
-// const diff = require('diff');
-
-// async function getArticleRevisions(title, startRevision, endRevision) {
-//   const apiUrl = `https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=${title}&rvprop=timestamp|user|content&rvstart=${startRevision}&rvend=${endRevision}&rvdir=newer&rvlimit=500`;
-
-//   try {
-//     const response = await axios.get(apiUrl);
-//     const pages = response.data.query.pages;
-
-//     // Extract revisions from the API response
-//     const revisions = Object.values(pages)[0]?.revisions || [];
-
-//     return revisions.map(revision => ({
-//       timestamp: revision.timestamp,
-//       user: revision.user || 'Anonymous',
-//       content: revision['*']
-//     }));
-//   } catch (error) {
-//     console.error('Error fetching data:', error.message);
-//     return [];
-//   }
-// }
-
-// function highlightChanges(previousContent, currentContent) {
-//   // ... (unchanged)
-// }
-
-// function saveRevisionsToFile(revisions, filename) {
-//   const formattedRevisions = revisions.map((revision, index) => {
-//     if (index > 0) {
-//       const previousContent = revisions[index - 1].content;
-//       revision.highlightedContent = highlightChanges(previousContent, revision.content);
-//     }
-//     return revision;
-//   });
-
-//   const data = JSON.stringify(formattedRevisions, null, 2);
-
-//   fs.writeFile(filename, data, (err) => {
-//     if (err) {
-//       console.error('Error saving revisions to file:', err.message);
-//     } else {
-//       console.log(`Revisions saved to ${filename}`);
-//     }
-//   });
-// }
-
-// const articleTitle = 'Ship_of_Theseus';
-// const startRevision = 351;
-// const endRevision = 850;
-// const outputFilename = 'ship_of_theseus_revisions.json';
-
-// (async () => {
-//   const revisions = await getArticleRevisions(articleTitle, startRevision, endRevision);
-//   saveRevisionsToFile(revisions, outputFilename);
-// })();
+  })
+})()
