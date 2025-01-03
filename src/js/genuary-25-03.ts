6fb3d9d349c87e5a3f95453a530dfb80f7e601de
David Ma <hello@david-ma.net>
2025-01-03 22:44:33 +1100

    Start adding a nav to genuary posts

--- src/js/genuary-25-03.ts
+++ src/js/genuary-25-03.ts
@@ -1,18 +1,10 @@
 import { Chart, d3 } from './chart'
 
-const width = 1920,
-  height = 1080
-
-$.when($.ready).then(function () {
-  const chart = new Chart({
-    element: 'datavizChart',
-    margin: 20,
-    width,
-    height,
-    nav: false,
-    renderer: 'canvas',
-  }).scratchpad(reset_chart)
-
+const chart = new Chart({
+  element: 'datavizChart',
+  nav: false,
+  renderer: 'canvas',
+}).scratchpad((chart) => {
   const message = [
     'I guess this is what 42 lines of code gets you.',
     'I hope you like it.',
@@ -23,20 +15,14 @@ $.when($.ready).then(function () {
 
   const text = chart.svg
     .append('g')
+    .classed('message', true)
     .selectAll('text')
     .data(message)
     .enter()
     .append('text')
     .text((d) => d)
     .attr('x', 80)
-    .attr('y', (d, i) => 80 + i * 80)
-    .attr('fill', 'white')
+    .attr('y', (d, i) => 120 + i * 80)
     .attr('font-family', 'm6x11')
-    .attr('font-size', '60px')
+    .attr('font-size', '42px')
 })
-
-function reset_chart(chart: Chart) {
-  chart.context.fillStyle = '#213'
-  chart.context.fillRect(0, 0, width, height)
-  chart.svg.selectAll('*').remove()
-}
