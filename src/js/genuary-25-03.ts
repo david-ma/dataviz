52a5e0da43be2e798f2d8dda146411c371b2b12a
David Ma <hello@david-ma.net>
2025-01-03 19:08:46 +1100

    Day 3

--- src/js/genuary-25-03.ts
+++ src/js/genuary-25-03.ts
@@ -13,13 +13,23 @@ $.when($.ready).then(function () {
     renderer: 'canvas',
   }).scratchpad(reset_chart)
 
-  const message = 'I guess this is what 42 lines of code gets you.'
+  const message = [
+    'I guess this is what 42 lines of code gets you.',
+    'I hope you like it.',
+    "Oh right, I'm supposed to be drawing something.",
+    'I guess I should start now.',
+    'Oops.',
+  ]
 
   const text = chart.svg
+    .append('g')
+    .selectAll('text')
+    .data(message)
+    .enter()
     .append('text')
-    .text(message)
+    .text((d) => d)
     .attr('x', 80)
-    .attr('y', 100)
+    .attr('y', (d, i) => 80 + i * 80)
     .attr('fill', 'white')
     .attr('font-family', 'm6x11')
     .attr('font-size', '60px')
