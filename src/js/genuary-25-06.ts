bc6f75d3aadcce4b5a7da779cd60f42fd52956cc
David Ma <hello@david-ma.net>
2025-01-09 11:15:58 +1100

    Use three js

--- src/js/genuary-25-06.ts
+++ src/js/genuary-25-06.ts
@@ -1,26 +1,45 @@
-import { d3 } from './chart'
-import {
-  Block,
-  Position,
-  RapierChart,
-  BlockOptions,
-  blockFactory,
-  ShapeType,
-} from './blocks'
+import { Chart, d3 } from './chart'
+import * as THREE from 'three'
 
-new RapierChart({
+new Chart({
   element: 'datavizChart',
   nav: false,
   renderer: 'canvas',
 })
   .clear_canvas()
-  .scratchpad((chart: RapierChart) => {
-    function render() {
-      chart.clear_canvas()
-      chart.world.step()
+  .scratchpad((chart: Chart) => {
+    // const width = window.innerWidth,
+    //   height = window.innerHeight
+    const width = chart.width,
+      height = chart.height
 
-      requestAnimationFrame(render)
+    // init
+
+    const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 10)
+    camera.position.z = 1
+
+    const scene = new THREE.Scene()
+
+    const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2)
+    const material = new THREE.MeshNormalMaterial()
+
+    const mesh = new THREE.Mesh(geometry, material)
+    scene.add(mesh)
+
+    const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true })
+    // renderer.setSize(width, height)
+    renderer.setAnimationLoop(animate)
+    // document.body.appendChild( renderer.domElement );
+    // #viz
+    document.getElementById('datavizChart').appendChild(renderer.domElement)
+    // d3.select("#datavizChart").html(renderer.domElement)
+
+    // animation
+
+    function animate(time) {
+      mesh.rotation.x = time / 2000
+      mesh.rotation.y = time / 1000
+
+      renderer.render(scene, camera)
     }
-
-    requestAnimationFrame(render)
   })
