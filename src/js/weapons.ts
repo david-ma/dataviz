197c88a5f279eb35bd0000470c9e3e8e2116556f
David Ma <hello@david-ma.net>
2025-10-30 16:49:50 +1100

    Weapons blogpost

---
+++ src/js/weapons.ts
@@ -0,0 +1,45 @@
+console.log("Running weapons.ts")
+
+const description = `#Weapons in flags
+
+There are 12 countries in the world with weapons in their flags.`
+
+// @ts-ignore
+var md = new showdown.Converter({ openLinksInNewWindow: true });
+
+$("#description").html(md.makeHtml(description))
+
+
+
+import { Chart, d3 } from './chart'
+
+
+const chart = new Chart({
+    element: 'chart',
+    margin: 20,
+    width: 1200,
+    height: 600,
+    nav: false,
+  }).initMap().then((chart: Chart) => {
+
+    chart.loadingAnimation.stop({goto: [0, 0]})
+    // console.log("loadingAnimation", chart.loadingAnimation)
+
+
+    chart.drawMap({
+      json: '/world-50.geo.json',
+      usa: '/gz_2010_us_040_00_5m.json',
+      aus: '/aust.json',
+      zoom: 200,
+      projection: d3.geoEquirectangular(),
+      // projection: d3.geoEqualEarth(),
+      calculate: () => {},
+      markers: [
+        {
+          longitude: 0,
+          latitude: 0,
+          type: 'Country',
+        }
+      ]
+    })
+  })
