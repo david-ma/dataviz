a36105bf46ccf75ceb8f4bbb2a97872401ea2414
David Ma <hello@david-ma.net>
2026-03-17 01:10:29 +1100

    Tidy up the config.ts

--- config/config.ts
+++ config/config.ts
@@ -1,15 +1,29 @@
-import { recursiveObjectMerge } from '../node_modules/thalia/server/website.js'
-import { gitHash } from './utilities.js'
 import path from 'path'
 import fs from 'fs'
-const fsPromise = fs.promises
+import type { RawWebsiteConfig } from 'thalia'
+import { recursiveObjectMerge } from '../node_modules/thalia/server/website.js'
+import { gitHash } from './utilities.js'
 import { blogposts, config as blogpostConfig } from './blogposts.js'
-let config = {
+
+const contentDirSegments = ['src', 'views', 'content'] as const
+const blogTemplateExtensions = ['.hbs', '.mustache'] as const
+
+const publishedBlogposts = blogposts.filter((post) => post.published)
+
+const hasBlogTemplate = (websiteRootPath: string, shortname: string) => {
+  const contentDir = path.resolve(websiteRootPath, ...contentDirSegments)
+
+  return blogTemplateExtensions.some((extension) =>
+    fs.existsSync(path.resolve(contentDir, `${shortname}${extension}`)),
+  )
+}
+
+let config: RawWebsiteConfig = {
   controllers: {
     '': (res, req, website, requestInfo) => {
       const html = website.getContentHtml('homepage')({
         gitHash,
-        blogposts,
+        blogposts: publishedBlogposts,
       })
       res.end(html)
     },
@@ -22,31 +36,23 @@ let config = {
         return
       }
 
-      // Get list of blogs by checking content
-      fsPromise
-        .readdir(path.resolve(website.rootPath, 'src', 'views', 'content'))
-        .then((files) => {
-          return files
-            .filter((file) => file.endsWith('.hbs') || file.endsWith('.mustache'))
-            .map((file) => file.replace('.hbs', '').replace('.mustache', ''))
-        })
-        .then((allowedBlogs) => {
-          if (!allowedBlogs.includes(shortname)) {
-            const html = website.getContentHtml('404', 'blog')({
-              gitHash,
-              blogposts,
-            })
-            res.end(html)
-            return
-          }
+      const blogpost = publishedBlogposts.find((post) => post.shortname === shortname)
 
-          const html = website.getContentHtml(shortname, 'blog')({
-            gitHash,
-            typescript: `/js/${shortname}.js`,
-            blogposts,
-          })
-          res.end(html)
+      if (!blogpost || !hasBlogTemplate(website.rootPath, shortname)) {
+        const html = website.getContentHtml('404', 'blog')({
+          gitHash,
+          blogposts: publishedBlogposts,
         })
+        res.end(html)
+        return
+      }
+
+      const html = website.getContentHtml(shortname, 'blog')({
+        gitHash,
+        typescript: `/js/${shortname}.js`,
+        blogposts: publishedBlogposts,
+      })
+      res.end(html)
     },
     source: (res, req, website, requestInfo) => {
       // Show the source code for a typescript file
@@ -75,9 +81,9 @@ let config = {
       }
       const filter = ['.DS_Store', '.gitignore', 'david.png', 'grace.png', 'index.html', 'printed']
       Promise.all([
-        fsPromise.readdir(path.resolve(basePath, 'az_images')),
-        fsPromise.readdir(path.resolve(basePath, 'ruby_images')),
-        fsPromise.readdir(path.resolve(basePath, 'renee_images')),
+        fs.promises.readdir(path.resolve(basePath, 'az_images')),
+        fs.promises.readdir(path.resolve(basePath, 'ruby_images')),
+        fs.promises.readdir(path.resolve(basePath, 'renee_images')),
       ]).then(function ([az, ruby, renee]) {
         var images = az
           .filter((d) => filter.indexOf(d) === -1)
@@ -89,16 +95,7 @@ let config = {
     },
   },
 }
-// if (fs.existsSync(path.resolve(__dirname, 'config.json'))) {
-//   config = _.merge(config, smugmugConfig)
-// } else {
-//   console.warn('config.json not provided, skipping smugmug stuff')
-// }
-// import { config as awesomeConfig } from './awesome'
-// config = _.merge(config, awesomeConfig)
-// import { config as cameraConfig } from './camera'
-// config = _.merge(config, cameraConfig)
-// import { config as atlassianConfig } from './atlassianBackend'
-// config = _.merge(config, atlassianConfig)
+
 config = recursiveObjectMerge(config, blogpostConfig)
+
 export { config }
